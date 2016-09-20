41bb793ad4c97ea6279af4dcd9695e04a49dfb49
jorp <jorp073@163.com>
2016-09-20 22:34:56 +0800

    get json data via jquery.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("123");
+﻿//alert("执行了js");
+
+function loadInfo() {
+    $.getJSON("data.json", function(data) {
+        $("#info").html("");//清空info内容
+        $.each(data.data, function(i, item) {
+            $("#info").append(
+                    "<div>" + item.img_url	+ "</div>" + 
+                    "<div>" + item.title	+ "</div>" +
+                    "<div>" + item.time		+ "</div>" + 
+					"<div>" + item.author	+ "</div>" +
+					"<div>" + item.pages	+ "</div><hr/>");
+        });
+        });
+};
+
+loadInfo();
